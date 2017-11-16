4a71a08b4b4225b377526c51c0618d158eb1fac9
Dave Moten <davidmoten@gmail.com>
2017-11-16 16:57:32 +1100

    dev

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl -H "x-api-key: $X_API_KEY" 'https://may066860m.execute-api.ap-southeast-2.amazonaws.com/dev/do?name=fred'
+curl -H "x-api-key: $X_API_KEY" 'https://9v4dgt3x6d.execute-api.ap-southeast-2.amazonaws.com/dev/do?name=fred'
 echo
