e746569489fd9a1b556e4e2a429bf7124c69a802
Dave Moten <davidmoten@gmail.com>
2021-05-13 09:48:14 +1000

    add s3 redirect example

--- src/main/java/com/github/davidmoten/apig/example/Handler.java
+++ src/main/java/com/github/davidmoten/apig/example/Handler.java
@@ -4,6 +4,9 @@ import java.util.Map;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.github.davidmoten.aws.helper.BadRequestException;
+import com.github.davidmoten.aws.helper.RedirectException;
+import com.github.davidmoten.aws.helper.ServerException;
 import com.github.davidmoten.aws.helper.StandardRequestBodyPassThrough;
 
 public class Handler implements RequestHandler<Map<String, Object>, String> {
@@ -14,14 +17,55 @@ public class Handler implements RequestHandler<Map<String, Object>, String> {
             // expects full request body passthrough from api gateway integration request
             StandardRequestBodyPassThrough request = StandardRequestBodyPassThrough.from(input);
             String name = request.queryStringParameter("name")
-                    .orElseThrow(() -> new IllegalArgumentException("parameter not found: 'name'"));
-            return "{\"response\": \"Hello " + name + "\"}";
+                    .orElseThrow(() -> new IllegalArgumentException("parameter 'name' not found"));
+
+            // demonstrate two paths
+            // 1 is s3 redirect
+            // 2 is return json
+            
+            if ("redirect".equals(name)) {
+                // return the s3 url of a file, return bucket should have expiry set to say 4
+                // hours so we have enough time to debug stuff if required
+//                String replyBucketName = "replyBucket";
+//                String replyObjectName = "replayObject";
+//                
+//                // use AWS SDK S3 object to generate presigned url
+//                String url = s3.generatePresignedUrl( //
+//                        replyBucketName, //
+//                        replyObjectName, //
+//                        new Date(System.currentTimeMillis() + TimeUnit.HOURS.toMillis(4))) //
+//                        .toString();
+                String url = "https://blah";
+
+                // must throw an exception to from java lambda to get 302
+                // redirection to work! The error message (the url) is mapped by
+                // the integration response part of the API Gateway to a 302
+                // status code with Location header equal to the url value
+                throw new RedirectException(url);
+            } else {
+                String json = "{\"response\": \"Hello " + name + "\"}";
+                // as we are returning JSON we specify a don't-touch-it response template in the
+                // cloudformation script so that it doesn't get serialized again into json
+                // (quoted).
+                return json;
+            }
         } catch (IllegalArgumentException e) {
-            // the error message prefix is used in cloudformation.yaml to map to a 400 status code
-            throw new RuntimeException("BadRequest: " + e.getMessage(), e);
+            // lambda infrastructure will ensure that thrown exception gets returned as a
+            // json payload {errorMessage, errorType, stackTrace, cause}
+
+            // cloudformation.yaml defines pattern to match the errorMessage field to a 400
+            // status code. Wrapping with BadRequestException will prefix the errorMessage
+            // with
+            // 'BadRequest' which is matched in cloudformation pattern.
+            throw new BadRequestException(e);
+        } catch (RedirectException e) {
+            // rethrow
+            throw e;
         } catch (Throwable e) {
-            // the error message prefix is used in cloudformation.yaml to map to a 500 status code
-            throw new RuntimeException("UnexpectedException: " + e.getMessage(), e);
+            /// cloudformation.yaml defines pattern to match the errorMessage field to a 500
+            // status code. Wrapping with ServerException will prefix the errorMessage with
+            // 'ServerException' which is matched in cloudformation pattern.
+            throw new ServerException(e);
         }
     }
 }
