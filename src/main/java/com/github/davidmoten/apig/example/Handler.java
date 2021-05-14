ddc998a9e6e9a351c151158fac67c2d994dc6d2a
Dave Moten <davidmoten@gmail.com>
2021-05-14 13:23:27 +1000

    rethrow standard exceptions

--- src/main/java/com/github/davidmoten/apig/example/Handler.java
+++ src/main/java/com/github/davidmoten/apig/example/Handler.java
@@ -17,11 +17,11 @@ public class Handler implements RequestHandler<Map<String, Object>, String> {
         try {
             // expects full request body passthrough from api gateway integration request
             StandardRequestBodyPassThrough request = StandardRequestBodyPassThrough.from(input);
-            
+
             // get the name query parameter
             String name = request.queryStringParameter("name")
                     .orElseThrow(() -> new IllegalArgumentException("parameter 'name' not found"));
-            
+
             // for a POST, request body is in input.get("body-json")
 
             // demonstrate two paths
@@ -57,7 +57,9 @@ public class Handler implements RequestHandler<Map<String, Object>, String> {
         }
         // lambda infrastructure will ensure that any thrown exception gets returned as
         // a json payload {errorMessage, errorType, stackTrace, cause}
-        catch (IllegalArgumentException e) {
+        catch (BadRequestException | ServerException | RedirectException e) {
+            throw e;
+        } catch (IllegalArgumentException e) {
             // Any exception that represents a bad request should be wrapped in a
             // BadRequestException and rethrown.
 
@@ -65,9 +67,6 @@ public class Handler implements RequestHandler<Map<String, Object>, String> {
             // status code. Wrapping with BadRequestException will prefix the errorMessage
             // with 'BadRequest' which is matched in cloudformation pattern.
             throw new BadRequestException(e);
-        } catch (RedirectException e) {
-            // rethrow
-            throw e;
         } catch (Throwable e) {
             // Note that it is advised to catch Throwable because any uncaught errors will
             // get mapped to a 200 response (assuming that their message doesn't by chance
